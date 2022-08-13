7dfea32845c411b06e54c16c0e2c206210a7438a
hanyeyu2 <109008173+hanyeyu2@users.noreply.github.com>
2022-08-14 01:23:27 +0800

    Update HW0807_02.java

--- HW0807/HW0807_02.java
+++ HW0807/HW0807_02.java
@@ -8,7 +8,7 @@ public class HW0807_02 extends HW0807_01 {
 	public static void main(String[] args) {
 		Scanner scan=new Scanner(System.in);
 		
-		System.out.print("請輸入欲求項次");
+		System.out.print("請輸入欲求項次總和");
 		int m =scan.nextInt();
 		double total = 0;
 		for(int i=1;i<=m+1;i++){
